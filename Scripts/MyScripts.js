93ef4665ddca4df7b753570b8016a9a27abc842d
Grineren <frederikboelskifte@gmail.com>
2017-07-21 16:26:18 +0200

    Adding javascripts

---
+++ Scripts/MyScripts.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+    
+    //fancybox
+    $("a#single_image").fancybox();
+
+
+    //Vis tekst ved klik på link 
+    $('.link').click(function () {
+        
+        var id = $(this).attr("rel");
+
+        $('#' + id).slideToggle('slow');
+    });
+
+});
---
+++ Scripts/RandomHeaderimg.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+
+
+
+    var images = ['headerimage1.jpg', 'headerimage2.jpg', 'headerimage3.jpg', 'headerimage4.jpg', 'headerimage5.jpg'];
+    $('header').css({ 'background-image': 'url(Billeder/header_images/' + images[Math.floor(Math.random() * images.length)] + ')' });
+
+
+
+});
